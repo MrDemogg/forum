1ada3ffcfe8db9b5646fbde26ac441f7a10b88f8
demog <dg08gg@mail.ru>
2022-12-10 20:29:29 +0300

    App.tsx
    Разделыл некоторые компоненты на разные страницы через роутеры

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Posts from "./components/Posts";
+import Description from "./components/Description";
+import CreatePosts from "./components/CreatePosts";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={'/post'} element={<Posts />} />
+        <Route path={'/post/:id'} element={<Description />} />
+        <Route path={'/create'} element={<CreatePosts />} />
+        <Route path={'*'} element={<Posts />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
