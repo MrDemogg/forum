df1c20285f281e5eab7915f89677ab3f25fab75b
demog <dg08gg@mail.ru>
2022-12-10 20:46:37 +0300

    NavBar.tsx
    init

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {Container, Nav, Navbar} from "react-bootstrap";
+import {useNavigate} from "react-router-dom";
 
 const NavBar = () => {
+  const router = useNavigate()
+
   return (
-    <div>
-      
-    </div>
+    <Navbar bg="dark" variant={'dark'} expand="lg">
+      <Container>
+        <Navbar.Brand onClick={() => router('/posts')}>Forum</Navbar.Brand>
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link onClick={() => router('/posts')}>Posts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
