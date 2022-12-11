b799c2bab5d69033464a222a6e5538edf4419003
demog <dg08gg@mail.ru>
2022-12-11 14:49:13 +0300

    Posts.tsx
    Сделал временный функцианал этого компонента и без стилизации

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {forumAPI} from "../service/ForumService";
+import {Alert, Box, LinearProgress} from "@mui/material";
 
 const Posts = () => {
+  const {data: posts, error, isError, isLoading, refetch} = forumAPI.useFindPostsQuery(undefined)
+  console.log(error)
+  useEffect(() => {
+    setInterval(() => {
+      refetch()
+    }, 60000)
+  }, [])
   return (
     <div>
-      
+      {isLoading
+        ? <Box sx={{ width: '100%' }}>
+            <LinearProgress />
+          </Box>
+        : isError
+          ? <Alert variant="filled" severity="error">
+              This is an error alert — check it out!
+            </Alert>
+          : posts && posts.length > 0
+            ? posts.map(post =>
+              <div key={post._id}>
+                <div>{post.title}</div>
+                <img src={post.image} alt={'img'} />
+                <div>{post.description}</div>
+              </div>
+            )
+            : <div>Ничего нет</div>
+      }
     </div>
   );
 };
