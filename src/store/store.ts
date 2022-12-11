faad9d3e35641e2508db9a60ea1ef2416ecd12a4
demog <dg08gg@mail.ru>
2022-12-11 11:25:08 +0300

    store.ts
    Подключил middleware

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,17 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import forumReducer from './reducers/ForumSlice'
+import {forumAPI} from "../service/ForumService";
 
 const rootReducer = combineReducers({
-
+  forumReducer,
+  [forumAPI.reducerPath]: forumAPI.reducer
 })
 
 export const setupStore = () => configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware()
+      .concat(forumAPI.middleware)
 })
 
 export type RootState = ReturnType<typeof rootReducer>
