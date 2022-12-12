11afaf7fccce172c28b83a22bb669bde54ece519
demog <dg08gg@mail.ru>
2022-12-12 09:46:50 +0300

    App.tsx
    Добавил взятие данных о пользователе (если таковые есть) из localStorage, чтобы ему не приходилось постоянно входить в аккаунт

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Posts from "./components/Posts";
 import Description from "./components/Description";
 import CreatePosts from "./components/CreatePosts";
+import NavBar from "./components/UI/NavBar";
+import {useAppDispatch} from "./hooks/redux";
+import {forumSlice} from "./store/reducers/ForumSlice";
 
 const App = () => {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const userData = localStorage.getItem('user')
+    if (userData) {
+      const parsedData = JSON.parse(userData)
+      if ('token' in parsedData && 'id' in parsedData && typeof parsedData.token === 'string' && typeof parsedData.id === "string") {
+        dispatch(forumSlice.actions.setToken(parsedData.token))
+        dispatch(forumSlice.actions.setId(parsedData.id))
+      }
+    }
+  }, [])
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path={'/post'} element={<Posts />} />
         <Route path={'/post/:id'} element={<Description />} />
