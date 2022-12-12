e35a875af01180aeec08f5e20958fb65c7d5b992
demog <dg08gg@mail.ru>
2022-12-12 09:51:26 +0300

    NavBar.tsx
    Изменил NavBar.tsx, подключив ProfileEntryModal.tsx и нужные кнопочки для работы некоторых функций. Logout и Post new Post ещё не работают

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
+import ProfileEntryModal from "../ProfileEntryModal";
+import {Button} from "@mui/material";
+import {useAppSelector} from "../../hooks/redux";
 
 const NavBar = () => {
   const router = useNavigate()
+  const [modalVisible, setModalVisible] = useState(false)
+  const {token} = useAppSelector(state => state.forumReducer)
 
   return (
-    <Navbar bg="dark" variant={'dark'} expand="lg">
-      <Container>
-        <Navbar.Brand onClick={() => router('/posts')}>Forum</Navbar.Brand>
-        <Navbar.Collapse>
-          <Nav className="me-auto">
-            <Nav.Link onClick={() => router('/posts')}>Posts</Nav.Link>
+    <div>
+      <Navbar bg="dark" variant='dark' expand='sm' className="mb-3 position-fixed w-100 top-0" style={{zIndex: 1}}>
+        <Navbar.Brand color={'#fff'} style={{marginLeft: 250}}>Forum</Navbar.Brand>
+        <Container fluid>
+          <Nav>
+            <Nav.Link onClick={() => router('/post')}>Posts</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          {token
+            ? <div style={{display: 'flex', flexDirection: 'row'}}>
+                <Button variant={'outlined'} color={'warning'} style={{marginRight: 20}}>Post new Post</Button>
+                <Button variant={'outlined'} color={'error'}>Logout</Button>
+              </div>
+            : <Button variant="outlined" color="info" onClick={() => setModalVisible(true)}>Login or Register</Button>
+          }
+          <ProfileEntryModal visible={modalVisible} changeVisible={setModalVisible} />
+        </Container>
+      </Navbar>
+    </div>
   );
 };
 
