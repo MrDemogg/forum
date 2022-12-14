f3a5755f02d4f3d6f7a427c55b77cd2309ff5595
demog <dg08gg@mail.ru>
2022-12-14 08:23:52 +0300

    Posts.tsx
    Убрал auto-refetch созданный через setInterval. Изменил и добавил стилей

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,24 +2,22 @@ import React, {useEffect} from 'react';
 import {forumAPI} from "../service/ForumService";
 import {
   Box,
-  Card,
+  Card, CardActionArea,
   CardContent,
   CardMedia,
   Typography
 } from "@mui/material";
 import {useAppDispatch, useAppSelector} from "../hooks/redux";
 import {forumSlice} from "../store/reducers/ForumSlice";
+import {useNavigate} from "react-router-dom";
 
 const Posts = () => {
   const {data: posts, error, isLoading, isError, refetch} = forumAPI.useFindPostsQuery(undefined)
-  const {globalIsLoading, globalIsError} = useAppSelector(state => state.forumReducer)
+  const {globalIsLoading, globalRefetch} = useAppSelector(state => state.forumReducer)
+  const router = useNavigate()
   const dispatch = useAppDispatch()
+  console.log(globalRefetch)
   useEffect(() => {
-    if (!globalIsError && !globalIsLoading) {
-      setInterval(() => {
-        refetch()
-      }, 200000)
-    }
     if (isError && error) {
       if ('originalStatus' in error && 'data' in error && error.originalStatus !== 200) {
         dispatch(forumSlice.actions.setError(error.data))
@@ -36,24 +34,34 @@ const Posts = () => {
     } else {
       dispatch(forumSlice.actions.changeGlobalIsLoading(false))
     }
-  }, [isError, isLoading])
+    if (globalRefetch) {
+      refetch()
+      dispatch(forumSlice.actions.globalRefetch(false))
+    }
+  }, [isError, isLoading, globalRefetch])
   dispatch(forumSlice.actions.changeGlobalIsError(isError))
   return (
-    <div style={{marginTop: 55}}>
-      {(!globalIsLoading && !globalIsLoading) && posts && posts.length > 0 && posts.map((post, index) =>
-              <Card sx={{ display: 'flex', width: '70%', margin: 'auto', marginTop: index > 0 ? 20 : 0 }} key={post._id}>
-                <CardMedia
-                  component="img"
-                  sx={{ width: 151 }}
-                  image={post.image}
-                  alt="Post image"
-                />
-                <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-                  <Typography variant={'h5'}>{post.datetime} by {post.username}</Typography>
-                  <CardContent>
-                    <Typography variant={'h2'}>{post.title}</Typography>
-                  </CardContent>
-                </Box>
+    <div style={{marginTop: 55, display: 'flex', flexDirection: 'column', width: '50%', margin: '0 auto'}}>
+      {(!globalIsLoading && !globalIsLoading) && posts && posts.length > 0 && posts.map(post =>
+              <Card sx={{ margin: '20px auto', width: '100%', backgroundColor: '#212121' }} key={post._id}>
+                <CardActionArea
+                 onClick={() => router('/posts/' + post._id + '?info=' +
+                   JSON.stringify({username: post.username, datetime: post.datetime, description: post.description, title: post.title}))}
+                 style={{display: 'flex', width: '100%', justifyContent: 'left'}}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{ width: 151, backgroundColor: post.image === "https://cdn-icons-png.flaticon.com/512/2639/2639965.png" ? '#fff' : '#212121' }}
+                    image={post.image}
+                    alt="Post image"
+                  />
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography sx={{color: '#eeeeee'}} variant={'h5'}>{post.datetime} by {post.username}</Typography>
+                    <CardContent>
+                      <Typography sx={{color: '#eeeeee'}} variant={'h2'}>{post.title}</Typography>
+                    </CardContent>
+                  </Box>
+                </CardActionArea>
               </Card>
             )
       }
