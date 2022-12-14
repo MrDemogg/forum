08a0a5780f7ddd901ce0f323573aa19436a4f562
demog <dg08gg@mail.ru>
2022-12-14 08:16:13 +0300

    Description.tsx
    Компонент для отображения описания поста и его комментариев

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useParams, useSearchParams} from "react-router-dom";
+import DescriptionCard from "./DescriptionCard";
+import Comments from "./Comments";
 
 const Description = () => {
+  const [searchParams] = useSearchParams()
+  const params = useParams()
+  const [postData, setPostData] = useState<{username: string, datetime: string, description: string, title: string}>()
+  useEffect(() => {
+    setPostData(JSON.parse(searchParams.get('info') as string))
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <>
+      {postData
+        &&  <div style={{marginTop: 55}}>
+              <DescriptionCard title={postData.title} username={postData.username} datetime={postData.datetime} text={postData.description} />
+              <Comments postId={params.id as string} />
+            </div>
+      }
+    </>
   );
 };
 
