d4eeabd05244d740fe22f7997a353540f7e4e565
demog <dg08gg@mail.ru>
2022-12-14 08:23:00 +0300

    NavBar.tsx
    Добавил кнопку refetch'а и функцианал кнопкам logout и Post new Post

--- src/components/UI/NavBar.tsx
+++ src/components/UI/NavBar.tsx
@@ -2,14 +2,28 @@ import React, {useState} from 'react';
 import {Container, Nav, Navbar} from "react-bootstrap";
 import {useNavigate} from "react-router-dom";
 import ProfileEntryModal from "../ProfileEntryModal";
-import {Button} from "@mui/material";
-import {useAppSelector} from "../../hooks/redux";
+import {Button, IconButton} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../../hooks/redux";
 import VisualInfo from "./VisualInfo";
+import {Refresh} from "@mui/icons-material";
+import {forumSlice} from "../../store/reducers/ForumSlice";
+import {forumAPI} from "../../service/ForumService";
 
 const NavBar = () => {
-  const router = useNavigate()
-  const [modalVisible, setModalVisible] = useState(false)
+  const [profileModalVisible, setProfileModalVisible] = useState(false)
   const {token, userId} = useAppSelector(state => state.forumReducer)
+  const [logout] = forumAPI.useLogoutMutation()
+  const dispatch = useAppDispatch()
+  const router = useNavigate()
+
+  const logoutHandler = () => {
+    token && userId && logout({token: token, userId: userId})
+      .then(() => {
+        dispatch(forumSlice.actions.setId(null))
+        dispatch(forumSlice.actions.setToken(null))
+        localStorage.removeItem('user')
+      })
+  }
 
   return (
     <div>
@@ -18,15 +32,20 @@ const NavBar = () => {
         <Container fluid>
           <Nav>
             <Nav.Link onClick={() => router('/post')}>Posts</Nav.Link>
+            <IconButton onClick={() => {
+              dispatch(forumSlice.actions.globalRefetch(true))
+            }}>
+              <Refresh color={'info'} />
+            </IconButton>
           </Nav>
           {token && userId
             ? <div style={{display: 'flex', flexDirection: 'row'}}>
-                <Button variant={'outlined'} color={'warning'} style={{marginRight: 20}}>Post new Post</Button>
-                <Button variant={'outlined'} color={'error'}>Logout</Button>
+                <Button variant={'outlined'} color={'warning'} style={{marginRight: 20}} onClick={() => router('/create')}>Post new Post</Button>
+                <Button variant={'outlined'} color={'error'} onClick={logoutHandler}>Logout</Button>
               </div>
-            : <Button variant="outlined" color="info" onClick={() => setModalVisible(true)}>Login or Register</Button>
+            : <Button variant="outlined" color="info" onClick={() => setProfileModalVisible(true)}>Login or Register</Button>
           }
-          <ProfileEntryModal visible={modalVisible} changeVisible={setModalVisible} />
+          <ProfileEntryModal visible={profileModalVisible} changeVisible={setProfileModalVisible} />
         </Container>
       </Navbar>
       <VisualInfo />
